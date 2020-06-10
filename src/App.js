ba4cdb35ce6cbce85789dc069352b262d9a3fff8
ichido <ichido@live.jp>
2020-06-10 17:58:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  input = '';
 
-export default App;
+  msgStyle = {
+    fontSize: "20pt",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+  }
+
+  inputStyle = {
+    fontSize: "12pt",
+    padding: "5px",
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      message: 'type your name:'
+    };
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      message: 'Hello, ' + this.input + '!!'
+    });
+    event.preventDefault();
+  }
+
+  render(){
+    return <div>
+      <h1>React</h1>
+      <h2>{this.state.message}</h2>
+      <form onSubmit={this.doSubmit}>
+        <label>
+          <span style={this.inputStyle}></span>Message:
+          <input type="text" style={this.inputStyle}
+            onChange={this.doChange} />
+        </label>
+        <input type="submit" style={this.inputStyle} value="Click" />
+      </form>
+    </div>
+  }
+}
